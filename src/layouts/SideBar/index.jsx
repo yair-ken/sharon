a11fd13eedaccd60d03715311d0fe5b9aae90d72
Yair Ken <yair.k.ken@gmail.com>
2023-02-12 19:39:58 +0200

    head and side-bar

--- src/layouts/SideBar/index.jsx
+++ src/layouts/SideBar/index.jsx
@@ -1,10 +1,10 @@
 import styles from "./style.module.css"
 
 export default function SideBar(){
-      let category = [`פ"ש`,`מועדים`,`דמויות מהתנ"ך`,`סיפורים`]
+      let category = [`זמן פ"ש`,`מועדים`,`דמויות מהתנ"ך`,`סיפורים`]
       return(
       <div>
-            {category.map(v=><div>{v}</div>)}
+            {category.map(v=><div key={v} className={styles.list}>{v}</div>)}
       </div>
       )
 }
